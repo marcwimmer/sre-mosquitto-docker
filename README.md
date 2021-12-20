3613fa448198f8fc7a3c796d3a358731383e133a
Your Name <you@example.com>
2021-12-20 20:36:32 +0000

    .

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-# slack_notifier
+slack_notifier
+=================
+
+Setup
+------
+
+/etc/sre/slack.conf:
+
+```yaml
+{
+"token": "T0M****************************************ODA",
+}
+```
+
+Usage
+----------
+
+```python
+client.publish("slack/notify", payload=b"this is a text")
 
---
+++ slack_notifier.py
@@ -0,0 +1,15 @@
+import requests
+import arrow
+import json
+from pathlib import Path
+
+def post_message_to_slack(text):
+    token = eval(Path("/etc/sre/slack.conf").read_text())['token']
+    return requests.post('https://hooks.slack.com/services/' + token, json={"text": text})
+
+
+def on_message(client, msg, payload=None):
+    if '/slack/notify' in msg.topic:
+        data = json.loads(msg.payload)
+        msg = f"{data['module']}: {data['value']}"
+        post_message_to_slack(msg)
